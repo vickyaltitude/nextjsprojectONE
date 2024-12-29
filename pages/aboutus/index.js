03755165138772f8aa9faccbe99791962bc1d3c6
vickyaltitude <vignvick3005>
2024-12-29 17:08:47 +0530

    Added dynamic page routing using Link tag of next/link

---
+++ pages/aboutus/index.js
@@ -0,0 +1,17 @@
+
+import Link from 'next/link';
+
+function DeveloperName() {
+  
+
+  return <>
+  <ul>
+  <li><Link href='/aboutus/1'> Hello world this is Yash</Link></li>
+  <li><Link href='/aboutus/2'> Hello world this is Vaibhav</Link></li>
+  <li><Link href='/aboutus/3'> Hello world this is Suresh</Link></li>
+  </ul>
+
+  </>;
+}
+
+export default DeveloperName;
