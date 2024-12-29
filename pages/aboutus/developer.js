26931a6394524e951a9483903085265d27a3e806
vickyaltitude <vignvick3005>
2024-12-29 10:40:43 +0530

    Made nested routes

---
+++ pages/aboutus/developer.js
@@ -0,0 +1,7 @@
+
+
+function Developer(){
+    return (<h1>Hello world this is Vignesh</h1>)
+}
+
+export default Developer
---
+++ pages/news/something-important.js
@@ -0,0 +1,7 @@
+
+
+function ImportantPage(){
+    return (<h1>Hello world this is ImportantPage</h1>)
+}
+
+export default ImportantPage
